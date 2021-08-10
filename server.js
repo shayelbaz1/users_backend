db0ba54074ee3826af1b1d58038db2766dd6bad0
shaysha6 <shaysha6@gmail.com>
2021-08-10 11:40:06 +0300

    3001

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','http://localhost:3001'],
         credentials: true
     };
     app.use(cors(corsOptions));
@@ -45,7 +45,7 @@ connectSockets(io)
 
 // Logging
 const logger = require('./services/logger.service')
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3000;
 http.listen(port, () => {
     logger.info('Server is running on port: ' + 'http://localhost:' + port)
     console.log('Server is running on port: ' + 'http://localhost:' + port)
