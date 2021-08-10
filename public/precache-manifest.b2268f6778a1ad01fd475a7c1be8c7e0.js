9be941ce64dd47dc0fc9e87e9d6a20451c54ed4d
shaysha6 <shaysha6@gmail.com>
2021-08-10 12:14:44 +0300

    add public

---
+++ public/precache-manifest.b2268f6778a1ad01fd475a7c1be8c7e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69adf08e422ba400ae810b8c7ca6baa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "389b26dfaeeeb228ecbc",
+    "url": "/static/css/2.61fb73e6.chunk.css"
+  },
+  {
+    "revision": "1bc973267cc62d4c15fa",
+    "url": "/static/css/main.dafef344.chunk.css"
+  },
+  {
+    "revision": "389b26dfaeeeb228ecbc",
+    "url": "/static/js/2.d6207e40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6207e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc973267cc62d4c15fa",
+    "url": "/static/js/main.3fec5ad6.chunk.js"
+  },
+  {
+    "revision": "b1e771b7abaad0775a5e",
+    "url": "/static/js/runtime-main.9c4303a4.js"
+  },
+  {
+    "revision": "db989291b2539b817e46ad20d4947c36",
+    "url": "/static/media/profile.db989291.png"
+  }
+]);
